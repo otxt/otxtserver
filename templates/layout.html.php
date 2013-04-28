e46978144d3e2914ddd4312b43e44c7338ae7560
sandeepshetty <sandeep.shetty@gmail.com>
2013-04-28 06:21:27 +0530

    Initial commit.

---
+++ templates/layout.html.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title><?php echo $title ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+</head>
+<body>
+	<?php echo $content ?>
+	<script src="js/jquery-1.9.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
